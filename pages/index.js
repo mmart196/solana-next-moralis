13c4c8116d83c109760b57601836472a432f97b3
Michael Martinez <mmart196@fiu.edu>
2022-05-24 18:49:35 -0500

    Added solana wallet and balance

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
+import { useMoralis } from "react-moralis";
 import Auth from "../components/Auth";
 import Dashboard from '../components/Dashboard'
 
 export default function Home() {
+
+  const {
+    isAuthenticated,
+    authenticate,
+    logout,
+    user
+  } = useMoralis()
+
+  if (!isAuthenticated){
+    return (
+      <Auth authenticate={authenticate}/>
+    )
+  }
+
   return (
     <>
-      {/* <Auth /> */}
-      <Dashboard />
+      <Dashboard logout={logout} user={user} />
     </>
   )
 }
